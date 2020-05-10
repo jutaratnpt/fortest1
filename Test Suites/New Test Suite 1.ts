9756bb5e93cbeaeed41e08d8c2ed7734534d20a4
TA <TA@HZTuktao>
2020-05-10 16:14:35 +0700

    Add test 2

---
+++ Test Suites/New Test Suite 1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5cd3f924-f8df-4b79-a8d3-e75c8371fd74</testSuiteGuid>
+</TestSuiteEntity>
